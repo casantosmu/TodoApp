f104bc6b6f3b1eaaef02530635aadefa2d275cde
Carlos Santos <casantosmu@gmail.com>
2024-02-15 21:43:17 +0100

    Modify Date to default Today

--- Models/Todo.cs
+++ Models/Todo.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace TodoApp.Models;
@@ -6,10 +7,12 @@ public class Todo
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    [DisplayName("Start")]
     [DataType(DataType.Time)]
     public DateTime? TimeIni { get; set; }
+    [DisplayName("End")]
     [DataType(DataType.Time)]
     public DateTime? TimeEnd { get; set; }
     [DataType(DataType.Date)]
-    public DateTime Date { get; set; }
+    public string Date { get; set; } = DateTime.Today.ToShortDateString();
 }
